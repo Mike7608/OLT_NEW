1ece743c9c9faacd9da0d4919edce5c5f3dfc61a
Mike Kushner <mike760807@gmail.com>
2024-04-08 22:57:45 +0300

    edit

--- OLT/OLT/tasks.py
+++ OLT/OLT/tasks.py
@@ -1,10 +1,9 @@
-# from datetime import timedelta
+from datetime import timedelta
 from celery import shared_task
 from django.core.mail import send_mail
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 from materials.models import Course, Subscription
-from dateutil.relativedelta import relativedelta
 
 
 @shared_task
@@ -12,7 +11,8 @@ def send_email_update_object(obj_id):
     course = Course.objects.get(id=obj_id)
 
     # Если прошло более 4 часов с момента последнего обновления
-    if timezone.now() - course.stamp_update > relativedelta(hours=4):
+
+    if timezone.now() - course.stamp_update > timedelta(hours=4):
         # Тогда отправляем сообщение
         send_mail(
             'Уведомление об обновлении курса',
@@ -32,8 +32,8 @@ def block_inactive_users():
     # Берем модель пользователя
     user = get_user_model()
 
-    # Берем период 1 месяц
-    period = timezone.now() - relativedelta(months=1)
+    # Берем период 30 дней
+    period = timezone.now() - timedelta(days=30)
 
     # Выбираем пользователей, которые не заходили в систему более указанного периода
     users = user.objects.filter(last_login__lte=period)
